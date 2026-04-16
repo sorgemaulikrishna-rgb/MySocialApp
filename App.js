446b9e6743167c61ddfccb7e955ab3ccf15bee49
maulisorgesorge-hue <maulisorgesorge@gmail.com>
2026-04-16 21:26:37 +0530

    Fix import statement for React in App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
